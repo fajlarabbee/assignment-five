4ee42da738a3592408a389fdd9775dc0c82ba43f
Fajla Rabbee <thewebdevonline@gmail.com>
2023-10-31 22:53:51 +0600

    Fixed index file

--- index.php
+++ index.php
@@ -1,12 +1,2 @@
 <?php
-//session_start();
-require_once __DIR__ . '/includes/functions.php';
-require_once __DIR__ . '/includes/classes/DB.php';
-
-//$db = new DB("config.json", "r");
-//
-//echo $db->read();
-
-getHeader();
-
-echo json_encode($_SERVER, JSON_PRETTY_PRINT);
+header('Location: login.php');
